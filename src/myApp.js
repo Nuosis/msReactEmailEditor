220063106f7bce721358fa9c541d7c26a475ad6e
Marcus Swift <marcus@claritybusinesssolutions.ca>
2023-10-09 13:46:32 -0300

    2023-09-10

--- src/myApp.js
+++ src/myApp.js
@@ -11,7 +11,10 @@ const MyApp = () => {
 
       unlayer?.exportHtml((data) => {
         const { design, html } = data;
-        console.log('exportHtml', html);
+        //console.log('exportHtml', html);
+        //console.log('designHtml', design);
+        FileMaker.PerformScript("receiveEmailHTML", JSON.stringify(html,design))
+        
       });
     };
 
@@ -27,11 +30,14 @@ const MyApp = () => {
 
     return (
       <div>
-        <div>
-          <button onClick={exportHtml}>Export HTML</button>
-        </div>
-
         <EmailEditor ref={emailEditorRef} onReady={onReady} />
+        <div>
+          <button class="m-8 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center" onClick={exportHtml}>
+            <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+            <span>Download</span>
+          </button>
+        </div>
+        
       </div>
     );
   };
