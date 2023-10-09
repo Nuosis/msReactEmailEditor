2b0166f017d77bbe5d96ca1ae4f2aaada76d39bf
Marcus Swift <marcus@claritybusinesssolutions.ca>
2023-10-09 19:04:26 -0300

    better output and onReady implemented

--- src/myApp.js
+++ src/myApp.js
@@ -1,5 +1,4 @@
 import React, { useRef } from 'react';
-import { render } from 'react-dom';
 
 import EmailEditor, { EditorRef, EmailEditorProps } from 'react-email-editor';
 
@@ -13,19 +12,19 @@ const MyApp = () => {
         const { design, html } = data;
         //console.log('exportHtml', html);
         //console.log('designHtml', design);
-        FileMaker.PerformScript("receiveEmailHTML", JSON.stringify(html,design))
-        
+        const output = {'json': design,'html': html}
+        FileMaker.PerformScript("receiveEmailHTML", JSON.stringify(output))
       });
     };
 
-    const onReady = (unlayer) => {
+    const onReady = (unlayer, json) => {
       // editor is ready
       // you can load your template here;
       // the design json can be obtained by calling
       // unlayer.loadDesign(callback) or unlayer.exportHtml(callback)
 
-      // const templateJson = { DESIGN JSON GOES HERE };
-      // unlayer.loadDesign(templateJson);
+      const templateJson = { json };
+      unlayer.loadDesign(templateJson);
     };
 
     return (
